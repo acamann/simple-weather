8ba7a55ed9e25f5a41eeaeeeb07d39b5842218a7
Andy Camann <acamann@gmail.com>
2024-11-02 15:35:33 -0500

    show temp for testing API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useGetCurrentWeatherQuery } from "./services/weather";
 
 function App() {
-  const { data: currentWeather, isLoading } = useGetCurrentWeatherQuery({
+  const { data: currentWeather } = useGetCurrentWeatherQuery({
     latitude: 29.8091725,
     longitude: -95.5404691,
   });
 
-  console.log(isLoading);
-  console.log(currentWeather);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Temp: {currentWeather?.main.temp}</h1>
     </>
   );
 }
