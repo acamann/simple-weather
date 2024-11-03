ba167ba24741fb0a593c5e65578c20d9d2ed9d60
Andy Camann <acamann@gmail.com>
2024-11-02 22:15:36 -0500

    styles and whatnot

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  flex: 1;
 `;
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Container>
         <CurrentWeatherView />
         <ForecastView />
+        <div>Andy Camann</div>
       </Container>
     </GeolocationProvider>
   );
