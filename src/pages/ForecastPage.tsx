3416e65d88799dd69c8b0f02c839a34b7110e3f9
Andy Camann <acamann@gmail.com>
2024-12-10 08:46:04 -0600

    show forecast data

--- src/pages/ForecastPage.tsx
+++ src/pages/ForecastPage.tsx
@@ -1,16 +1,31 @@
 import styled from "styled-components";
-import { ForecastView } from "../components/ForecastView";
 import { BackButton } from "../components/BackButton";
+import { useContext } from "react";
+import { WeatherContext } from "../providers/WeatherContext";
+import { ForecastGraph } from "../components/ForecastGraph";
 
 const Container = styled.div`
   width: 100%;
 `;
 
 export const ForecastPage = () => {
+  const { forecast } = useContext(WeatherContext);
   return (
     <Container>
       <BackButton path="/" />
-      <ForecastView />
+      <ForecastGraph forecast={forecast.list} />
+      <table style={{ width: "100%", paddingTop: 16 }}>
+        {forecast.list.map((item) => (
+          <tr key={item.dt}>
+            <td>{item.dt}</td>
+            <td>{item.main.temp}</td>
+            <td>{item.main.feels_like}</td>
+            <td>{item.pop}</td>
+            <td>{item.rain?.["3h"]}</td>
+            <td>{item.weather[0].main}</td>
+          </tr>
+        ))}
+      </table>
     </Container>
   );
 };
